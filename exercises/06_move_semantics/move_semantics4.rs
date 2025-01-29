b532d19b713d74688603d74f7a277711c10b9f57
AndreasNelson <nel02254@morris.umn.edu>
2025-01-29 11:57:05 -0600

    completed exercise 6

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -8,11 +8,11 @@ mod tests {
     // Don't add, change or remove any line.
     #[test]
     fn move_semantics4() {
-        let mut x = Vec::new();
-        let y = &mut x;
-        let z = &mut x;
-        y.push(42);
-        z.push(13);
-        assert_eq!(x, [42, 13]);
+    let mut x = Vec::new();
+    let y = &mut x;
+    y.push(42);
+    let z = &mut x;
+    z.push(13);
+    assert_eq!(x, [42, 13]);
     }
 }
